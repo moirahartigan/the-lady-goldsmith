39f65ec5bace167ad39ddbd6112ccb1d4a0dd9fd
moirahartigan <moirahartigan@gmail.com>
2021-12-19 15:30:16 +0000

    Updated the context processor

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,31 +1,39 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 def bag_contents(request):
     """
-    This is a context processor.
-    Its purpose is to make this dictionary available to all templates across the entire application
-    Don't forget to add this to the context processors in settings.py to make it available throughout the app
-    """
+      This is a context processor.
+      Its purpose is to make this dictionary available to all templates across the entire application
+      Don't forget to add this to the context processors in settings.py to make it available throughout the app
+      """
 
-    # List for the bag items to live 
     bag_items = []
-    # starting total 
     total = 0
-    # empty basket
     product_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, item_data in bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += item_data * product.price
+        product_count += item_data
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': item_data,
+            'product': product,
+        })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
-        # Decimal is less susceptible to rounding errors than using Float. Always use this for money
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
-        # Let user how much more they need for free shipping
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
         delivery = 0
         free_delivery_delta = 0
-    
+
     grand_total = delivery + total
-    
+
     context = {
         'bag_items': bag_items,
         'total': total,
