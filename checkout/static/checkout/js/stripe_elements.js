1c4ae623562ab76033ef80f93010db1a0f85cf6e
moirahartigan <moirahartigan@gmail.com>
2021-12-20 19:22:01 +0000

    Add basic checkout functionality, test payment successful

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -28,5 +28,53 @@ var style = {
 };
 // create a card element
 var card = elements.create('card', {style: style});
-// and mount the card element to the div we created 
 card.mount('#card-element');
+
+// Handle realtime validation errors on the card element
+card.addEventListener('change', function (event) {
+// add listener to card element and check for errors everytime it changes 
+var errorDiv = document.getElementById('card-errors');
+if (event.error) {
+    // If there are, display them in the card errors div 
+    var html = `
+        <span class="icon" role="alert">
+            <i class="fas fa-times"></i>
+        </span>
+        <span>${event.error.message}</span>
+    `;
+    $(errorDiv).html(html);
+} else {
+    // otherwise it will be blank 
+    errorDiv.textContent = '';
+}
+});
+
+// Handle form submit
+var form = document.getElementById('payment-form');
+
+form.addEventListener('submit', function(ev) {
+    ev.preventDefault();
+    card.update({ 'disabled': true});
+    $('#submit-button').attr('disabled', true);
+    stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: card,
+        }
+    }).then(function(result) {
+        if (result.error) {
+            var errorDiv = document.getElementById('card-errors');
+            var html = `
+                <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+                </span>
+                <span>${result.error.message}</span>`;
+            $(errorDiv).html(html);
+            card.update({ 'disabled': false});
+            $('#submit-button').attr('disabled', false);
+        } else {
+            if (result.paymentIntent.status === 'succeeded') {
+                form.submit();
+            }
+        }
+    });
+});
--- checkout/views.py
+++ checkout/views.py
@@ -1,24 +1,48 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
 
+from bag.contexts import bag_contents
+
+import stripe
+
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     bag = request.session.get('bag', {})
     # if bag is empty
     if not bag:
         messages.error(request, "There's nothing in your bag at the moment")
-        # redirect back to the checkout page 
+        # redirect back to the checkout page
         return redirect(reverse('products'))
 
-    # create empty instance of order form 
+    # for stripe
+    current_bag = bag_contents(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
+    # create empty instance of order form
     order_form = OrderForm()
+    # message incase you forget to set secret key 
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
+
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51JxVZlDaCUI9Xs6pYLpQtpPpwXkajedKGkISekzYYszDhKvkXREIGFq08YyaESkDZBXk0dGlkjwVb5wmpkeSeRR000aaxfuiCJ',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
