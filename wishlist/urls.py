e84c492bb6d40b09335ef4eb2573fc1e2ec2220d
moirahartigan <moirahartigan@gmail.com>
2022-01-07 13:12:33 +0000

    Add remove and add to wishlist functionality to wishlist

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('view_product_wishlist/', views.view_product_wishlist, name='view_product_wishlist')
+    path('view_product_wishlist/', views.view_product_wishlist, name='view_product_wishlist'),
+    path('add_product_to_wishlist/<item_id>/', views.add_product_to_wishlist, name='add_product_to_wishlist'),
+    path('remove_product_from_wishlist/<item_id>/', views.remove_product_from_wishlist,
+         name='remove_product_from_wishlist'),
+
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,12 +1,11 @@
-from django.shortcuts import render
-
+from django.http import Http404
+from django.shortcuts import (render, get_object_or_404, redirect)
 from django.contrib import messages
 
 from django.urls import reverse
 
 
 from products.models import Product
-from util.util import setup_pagination
 from .models import Wishlist
 
 
@@ -19,15 +18,12 @@ def view_product_wishlist(request):
     Returns:
         Renders the request, template and context
     """
-    wishlist_items_count = 0
     try:
         all_wishlist = Wishlist.objects.filter(username=request.user.id)[0]
     except IndexError:
         wishlist_items = None
     else:
         wishlist_items = all_wishlist.products.all()
-        wishlist_items_count = all_favourites.products.all().count()
-        wishlist_items = setup_pagination(favourites_items, request, 4)
 
     if not wishlist_items:
         messages.info(request, 'Your wishlist list is empty!')
@@ -35,6 +31,36 @@ def view_product_wishlist(request):
     template = 'wishlist/wishlist.html'
     context = {
         'wishlist_items': wishlist_items,
-        'wishlist_items_count': wishlist_items_count
     }
     return render(request, template, context)
+
+
+def add_product_to_wishlist(request, item_id):
+    """
+    Add a product item to wishlist
+    """
+    product = get_object_or_404(Product, pk=item_id)
+    try:
+        wishlist = get_object_or_404(Wishlist, username=request.user.id)
+    except Http404:
+        wishlist = Wishlist.objects.create(username=request.user)
+    if product in wishlist.products.all():
+        messages.info(request, 'The product item is already in your wishlist!')
+    else:
+        wishlist.products.add(product)
+        messages.info(request, 'Added product item to your wishlist')
+    return redirect(reverse('product_detail', args=[item_id]))
+
+
+def remove_product_from_wishlist(request, item_id):
+    """
+    Remove a product item from wishlist
+    """
+    product = get_object_or_404(Product, pk=item_id)
+    wishlist = get_object_or_404(Wishlist, username=request.user.id)
+    if product in wishlist.products.all():
+        wishlist.products.remove(product)
+        messages.info(request, 'Product item removed from your wishlist list')
+    else:
+        messages.error(request, 'That product item is not in your wishlist list!')
+    return redirect(reverse('product_detail', args=[item_id]))
