f19be4c8815012dec2a3c6e797a50c993e8b5a12
moirahartigan <moirahartigan@gmail.com>
2022-01-13 12:18:28 +0000

    Redo deployment part 1 with correct  database url

--- the_lady_goldsmith/settings.py
+++ the_lady_goldsmith/settings.py
@@ -100,17 +100,17 @@ MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 WSGI_APPLICATION = 'the_lady_goldsmith.wsgi.application'
 
 
-if 'DATABASE_URL' in os.environ:
-      DATABASES = {
-          'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-      }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
+# if 'DATABASE_URL' in os.environ:
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+}
+# else:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         }
+#     }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
