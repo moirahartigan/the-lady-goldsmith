d6ca5b37e4a978c4d7998bc8d2ede737046790eb
moirahartigan <moirahartigan@gmail.com>
2022-01-18 19:26:11 +0000

    Adjust wishlist html to match products/sale pages

--- wishlist/admin.py
+++ wishlist/admin.py
@@ -7,6 +7,13 @@ class WishlistAdmin(admin.ModelAdmin):
     list_display = (
         'username',
     )
+    search_fields = (
+        'username',
+    )
+    list_filter = (
+        'username',
+    )
+    list_per_page = 20
 
 
 admin.site.register(Wishlist, WishlistAdmin)
