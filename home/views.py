c0e0573627acc78a2cf1ff69fb4a3b04b118ffb8
moirahartigan <moirahartigan@gmail.com>
2022-01-20 19:29:29 +0000

    Add button to carousel and link categories

--- home/views.py
+++ home/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from products.models import Product
 
 
 def index(request):
     """ A View to return the index page """
-    print("Tried to run")
+
+    shop_home = Product.objects.all()
+
+    context = {
+        'shop_home': shop_home,
+    }
     return render(request, 'home/index.html')
