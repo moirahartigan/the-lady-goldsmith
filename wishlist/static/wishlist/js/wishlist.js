40e84689056072277dbf7f4ecde95f8263405319
moirahartigan <moirahartigan@gmail.com>
2022-01-27 10:03:52 +0000

    Add wishlist option to products pg and remove unused js

--- wishlist/static/wishlist/js/wishlist.js
+++ wishlist/static/wishlist/js/wishlist.js
@@ -5,23 +5,6 @@
 $('.btt-link').click(function(e) {
     window.scrollTo(0, 0);
 });
-$('#sort-selector').change(function() {
-    var selector = $(this);
-    var currentUrl = new URL(window.location);
-    var selectedVal = selector.val();
-    if (selectedVal != "reset") {
-        var sort = selectedVal.split("_")[0];
-        var direction = selectedVal.split("_")[1];
-        currentUrl.searchParams.set("sort", sort);
-        currentUrl.searchParams.set("direction", direction);
-        window.location.replace(currentUrl);
-    } else {
-        currentUrl.searchParams.delete("sort");
-        currentUrl.searchParams.delete("direction");
-        window.location.replace(currentUrl);
-    }
-});
-
 
 $('#myModal').on('shown.bs.modal', function() {
     $('#myInput').trigger('focus');
